1e752464877fdf7eab315595c9d5287439b3689b
ngxson <thichthat@gmail.com>
2021-10-19 16:03:10 +0200

    add files

---
+++ scripts/build-files-directory.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const DIR_FW = '../public/files/fw';
+const DIR_RES = '../public/files/res';
+const DIR_APP = '../public/files/app';
+const DIR_LIBBIP = '../public/files/libbip';
+const OUTPUT_DIR = '../src/data/files.json';
+
+function run() {
+  processDir(DIR_FW, 'fw');
+  processDir(DIR_RES, 'res');
+  processDir(DIR_APP, 'app');
+  processDir(DIR_LIBBIP, 'libbip');
+  const newContent = JSON.stringify(content, null, 2);
+  fs.writeFileSync(path.join(__dirname, OUTPUT_DIR), newContent);
+}
+
+const content = JSON.parse(
+  fs.readFileSync(path.join(__dirname, OUTPUT_DIR)).toString()
+);
+const getFileList = (relativePath) => fs.readdirSync(path.join(__dirname, relativePath));
+const processDir = (relativePath, label) => {
+  if (!content[label]) content[label] = {};
+  const files = getFileList(relativePath);
+  for (const file of files) {
+    if (!content[label][file]) content[label][file] = {};
+  }
+};
+
+run();
--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import { getResData } from './utils/res-extract';
-import { downloadArrayBuffer, getResBinary } from './utils/res-pack';
+import './bootstrap.min.css';
 
 function App() {
-  const test = async function () {
-    const resData = await getResData('not_latin_1.1.2.05_ENGLISH');
-    const newResBin = await getResBinary(resData);
-    downloadArrayBuffer(newResBin, 'not_latin_1.1.2.05_ENGLISH_packed.res');
-  };
-
   return (
     <div>
-      <button onClick={test}>TESTTTTT</button>
     </div>
   );
 }
